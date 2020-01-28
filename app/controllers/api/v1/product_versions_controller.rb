24b8a8fdf29e9967d9840b9a2958e0d3631a35a0
Twinkle Pardeshi <tpardesh@redhat.com>
2020-01-28 17:43:07 +0530

    Rename version controller to product_version controller

---
+++ app/controllers/api/v1/product_versions_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # Controller methods for product_version model
+    class ProductVersionsController < ApplicationController
+      before_action :get_products_id
+      def index
+        @versions = @product.product_versions
+        render json: @versions
+      end
+
+      private
+      def get_products_id
+        @product = Product.find(params[:product_id])
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  match ':controller(/:action(/:id))', :via => [:get, :post]
   namespace 'api' do
     namespace 'v1' do
-      resources :products
+      resources :products do
+        resources :product_versions
+      end
     end
   end
 end
